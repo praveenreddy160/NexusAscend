4e03e2c125572eb692d2f9870898790ff931c781
Praveen Reddy Peddi <praveenreddy160@gmail.com>
2024-10-28 13:22:36 -0700

    Band Generator-code

---
+++ python_course.py
@@ -0,0 +1,8 @@
+# printing the len of input and also having the input in the print function
+print(len("Hello " + input("Please enter your name:\n") + "!"))
+
+# project-1 Band Generator taking the input from the user and printing the band name 
+print("welcome to the Band Name Generator.")
+city = input("enter the city where you are born?\n")
+pet = input("enter your pet name?\n")
+print("your band name might be: " + city + " " + pet)
