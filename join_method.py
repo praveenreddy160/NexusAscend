2464078bfacd42291f4e413dada5ea93e8565306
Praveen Reddy Peddi <praveenreddy160@gmail.com>
2024-11-06 21:59:02 -0700

    love_score code

--- join_method.py
+++ join_method.py
@@ -1,5 +1,30 @@
-Leterrs = ['a', 'b', 'c', 'd']
-new_form = '-'.join(Leterrs)
-print(new_form)
-# "" is just a seperator, if you put a "- then it will join with a- b-".   "" this doesnt exist in result its imaginable understand
+# Letters = ['a', 'b', 'c', 'd']
+# new_form = '-'.join(Letters)
+# print(new_form)
 
+
+
+
+def calculate_love_score(name1, name2):
+
+    # Combined the names and convert to lowercase
+    combined_names = name1.lower() + name2.lower()
+    
+    # Initializing counters for TRUE and LOVE letters
+    true_count = 0
+    love_count = 0
+
+    # Count occurrences of letters in "TRUE"
+    for letter in "true":
+        true_count += combined_names.count(letter)
+
+    
+    for letter in "love":
+        love_count += combined_names.count(letter)
+
+    score = int(str(true_count) + str(love_count))
+    
+    print(f"Your love score is {score}")
+
+
+calculate_love_score('Kanye West', 'Kim Kardashian')
