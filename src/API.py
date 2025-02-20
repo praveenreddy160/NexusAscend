400049808ef460d3666ffb7e2e7c8d8f3f76754e
Praveen Reddy Peddi <praveenreddy160@gmail.com>
2025-02-19 22:32:50 -0700

    ISS tracking notifying task

--- src/API.py
+++ src/API.py
@@ -6,9 +6,19 @@ import requests
 # latitude = data["latitude"]
 # print(longitude, latitude)
 
-def get_quote():
-    response = requests.get(url="https://api.kanye.rest")
-    quotes = response.json()
-    data = quotes["quote"]
-    print(data)
-get_quote()
+def my_location():
+    my_lax = 17.385044
+    my_lng = 78.486671
+    Parameters = {
+        "lat": my_lax,
+        "lng": my_lng,
+        "formatted": 0  # Use 0 for unformatted (ISO 8601) times, 1 for formatted times
+    }
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=Parameters)
+    response.raise_for_status()
+    data = response.json()
+    sun_rise = data["results"] ["sunrise"]
+    sun_set = data ["results"] [ "sunset"]
+    print(sun_rise, sun_set)
+
+my_location()
